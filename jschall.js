40cd645fb1ac9a6bfc6f1fb746f0c37cc6d120ad
shwetarimi <shweta.rimi123@gmail.com>
2021-01-20 23:35:07 +0530

    mouseevent

---
+++ jschall.js
@@ -0,0 +1,7 @@
+let array=[1,2];
+let newArray=array;
+let arr=[1,2];
+console.log(array==array);
+console.log(array==newArray);
+console.log(array==arr);
+console.log(arr==newArray);
---
+++ jschall2.js
@@ -0,0 +1,9 @@
+function add(val) {
+    return val + 10;
+}
+let multi=function(val){
+    return val * 10;
+}
+console.log(add(2));
+console.log(multi(2));
+console.log(add(multi(2)));
